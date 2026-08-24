c2d74664ec70008127fb0d7c9d1e2bd006584cb4
Douglass <2875105710@qq.com>
2026-08-25 00:44:38 +0800

    修改Base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/shou-guide/',
   title: "SHOU指北",
   description: "A VitePress Site",
   themeConfig: {
